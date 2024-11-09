a6a4332062759851096afedc5e3225842897d2ac
Meybili <meybili200219@gmail.com>
2024-11-09 16:39:52 -0500

    Agregar archivos de proyecto.

---
+++ exercise2csharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exercise2csharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
